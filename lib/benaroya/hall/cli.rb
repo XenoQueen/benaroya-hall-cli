9034b6d9a8373d957d6ba1b893efe54deb962122
Maika Kailani <queenmab051@gmail.com>
2020-01-13 20:18:51 +0000

    Benaroya cli

--- lib/benaroya/hall/cli.rb
+++ lib/benaroya/hall/cli.rb
@@ -1,10 +1,45 @@
 require "benaroya/hall/cli/version"
 
-module Benaroya
-  module Hall
-    module Cli
-      class Error < StandardError; end
-      # Your code goes here...
-    end
+class Benaroya::CLI
+  def call
+    Benaroya::Scraper.scrape
+    welcome
+    menu
+    list_concerts
+    buy_tickets
   end
+  
+  def welcome
+    puts ""
+    puts "Welcome to Benaroya Hall, home of the Seattle Symphony!"
+    puts ""
+    puts "To view upcoming concerts and events, type 'list'."
+    puts "To purchase tickets, type 'ticket'."
+    puts "To leave this "
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# module Benaroya
+#   module Hall
+#     module Cli
+#       class Error < StandardError; end
+#       # Your code goes here...
+#     end
+#   end
+# end
