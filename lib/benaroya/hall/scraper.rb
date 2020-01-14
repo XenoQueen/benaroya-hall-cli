9dde56b04e255b8dc74244f7a10b418d2e4bc1d5
Maika Kailani <queenmab051@gmail.com>
2020-01-14 19:18:00 +0000

    Added scraper.rb

---
+++ lib/benaroya/hall/scraper.rb
@@ -0,0 +1,19 @@
+require 'open-uri'
+require 'pry'
+
+class Scraper
+  
+  attr_accessor :name, :date, :tickets
+  
+  def self.scrape_index_page(index_url) #method that scrapes website
+    
+    
+    
+  end
+  
+  def self.scrape_concert_page(concert_url) #method that scrapes individual concerts
+    
+    
+    
+    
+  end
