e9fc641a8ef628b64e9c04986a9ab715b4e605ab
Maika Kailani <queenmab051@gmail.com>
2020-01-22 17:54:56 +0000

    Added environment.rb file

---
+++ config/environment.rb
@@ -0,0 +1,8 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+require relative '../lib/benaroya/hall/scraper'
+require relative '../lib/benaroya/hall/concert'
+require relative '../lib/benaroya/hall/cli'
+require relative '../lib/benaroya/hall/cli/version'
