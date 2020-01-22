25a25b24c991df8da2390983ae27f91f5dd92a88
Maika Kailani <queenmab051@gmail.com>
2020-01-22 19:07:14 +0000

    Updated scraper.rb, removed unecessary method

--- lib/benaroya/hall/scraper.rb
+++ lib/benaroya/hall/scraper.rb
@@ -1,19 +1,13 @@
 require 'open-uri'
 require 'pry'
 
-class Scraper
+class Benaroya::Scraper
   
-  attr_accessor :name, :date, :tickets
+  attr_accessor :name, :date, :time, :tickets
   
-  def self.scrape_index_page(index_url) #method that scrapes website
-    
+  def self.scrape_index_page(index_url) #method that scrapes website for concerts and events
+    doc = Nokogiri::HTML(open("https://www.seattlesymphony.org/benaroyahall/concerts-tickets"))
     
     
   end
-  
-  def self.scrape_concert_page(concert_url) #method that scrapes individual concerts
-    
-    
-    
-    
-  end
+end
